975addeb3ff91901c8c29bd47829318a30796e5b
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-04-25 12:47:15 -0700

    Project 1

--- week2/project1/oddOrEven.py
+++ week2/project1/oddOrEven.py
@@ -1,8 +1,7 @@
-def oddOrEven(val):
+def even(val):
     if(int(val) % 2 == 0):
-        print("You entered an even number.")
-    else:
-        print("You entered an odd number.")
+        return True
+    return False
 
 def multOfFour(val):
     if(int(val) % 4 == 0):
@@ -11,21 +10,25 @@ def multOfFour(val):
 
 def evenDiv(val1, val2):
     if(int(val1) % int(val2) == 0):
-        print(val2 + " divides evently into " + val1)
-    else:
-        print(val2 + " does not divide evently into " + val1)
+        return True
+    return False
 
 def project1():
     num = input("Enter a number: ")
 
     if(multOfFour(num)):
         print(num + " is a multiple of 4.")
+    elif(even(num)):
+        print("You entered an even number.")
     else:
-        oddOrEven(num)
+        print("You entered an odd number.")
 
     num = input("Enter a number: ")
     check = input("Enter a second number: ")
 
-    evenDiv(num, check)
+    if(evenDiv(num, check)):
+        print(check + " divides evently into " + num)
+    else:
+        print(check + " does not divide evently into " + num)
 
 project1()
