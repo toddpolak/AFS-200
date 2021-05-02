9ecc1c2c3c9f6536ed65da81fe9568296ef7cb29
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-05-02 00:15:28 -0700

    Tuples_Joins_Splits

--- week3/join_split/join_split.py
+++ week3/join_split/join_split.py
@@ -1,8 +1,28 @@
-csv = 'Eric,John,Michael,Terry,Graham,TerryG,Brian'
-friends_list = []
-friends = csv.split(',')
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
 
-for friend in friends:
-    friends_list.append(friend)
+print('csv: ', csv)
 
-print(friends_list)
+friends1 = csv.split(',')
+
+print('friends1: ', friends1)
+
+friends_list = friends1[:4]
+
+friends2 = friends1[4]
+
+print('friends2: ', friends2)
+
+friends3 = friends2.split(':')
+
+print('friends3: ', friends3)
+
+friends_list.append(friends3[0])
+
+friends4 = friends3[1].split(';')
+
+print('friends4: ', friends4)
+
+for elem in friends4:
+    friends_list.append(elem)
+
+print('friends_list: ', friends_list)
--- week3/tuple/tuple.py
+++ week3/tuple/tuple.py
@@ -1,4 +1,4 @@
-tup1 = ('Robert', 'Robert Smith', 1970, ['War Games', 1983], 'Web Developer', 'San Diego, CA')
+tup1 = ('Robert', 'Smith', 1970, ['War Games', 1983], 'Web Developer', 'San Diego, CA')
 tup2 = (1,2,3,4,5,6,7)
 
 print(tup1[0])
