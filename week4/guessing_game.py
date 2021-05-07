642b9f4e82982bc64a0a42cbab1a855ae610a3a8
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-05-07 13:03:24 -0700

    Project 2 Guessing Game One

--- week4/guessing_game.py
+++ week4/guessing_game.py
@@ -3,19 +3,17 @@ import random
 random_numnber = int(random.randint(1, 9))
 
 print('############################################################')
-print('#          Welcome to the Number Guessing Game             #')
+print('                   Number Guessing Game                     ')
 print('############################################################')
 
 player_name = input('Enter your name: ')
 
 print('Hi, {}.'.format(player_name))
 
-player_input = input('To begin, press Enter. To quit any time, type "exit" ')
+player_input = input('Hit any key to begin. To quit any time, type "exit" ')
 
 while player_input.lower() != 'exit':
-
     try:
-
         player_input = input('Pick a number between 1 and 9: ')
 
         if int(player_input) < 1 or int(player_input) > 9:
@@ -30,17 +28,20 @@ while player_input.lower() != 'exit':
             random_numnber = int(random.randint(1, 9))
 
             print('Let''s play again ... ')
-            player_input = input('Pick a number between 1 and 9: ')
+
+            continue
 
         elif int(player_input) > random_numnber:
             print('Guess lower .. ')
+
         elif int(player_input) < random_numnber:
             print('Guess higher .. ')
 
     except ValueError as err:
-        print('Invalid entry ... ')
-    
-        
+        if player_input.lower() == 'exit':
+            print("Thank you for playing. Good bye!")
+            break
 
+        print('Invalid entry ... ')
 else:
     print("Thank you for playing. Good bye!")
