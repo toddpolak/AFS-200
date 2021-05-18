8ac8f22064ed6ac459f75f8e946815ab59d3a755
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-05-17 17:28:08 -0700

    Strings and Calculations

--- week5/lambda/lambda.py
+++ week5/lambda/lambda.py
@@ -1,10 +1,14 @@
 #Function that takes one argument, and that argument will be multiplied with an unknown given number
 
+#Option 1
+
 multiplier = 360
 
 multiply = lambda x: x * multiplier
 
 print(multiply (5))
+
+#Option 2
 
 def lambdaMultiplier(num):
     return lambda x : x * num
