e77a531dca987badfa53f3b6b252e8e4559067e7
toddpolak <67641770+toddpolak@users.noreply.github.com>
2021-06-01 09:57:25 -0700

    Capstone Project

--- week7/projects/django_project/django_database/models.py
+++ week7/projects/django_project/django_database/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+#class SampleData(models.Model):
+    #item = models.CharField(max_length=100)
--- week7/projects/django_project/django_project/settings.py
+++ week7/projects/django_project/django_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_database',
+    #'accounts',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Django_DB_Project',
+        'USER': 'postgres',
+        'PASSWORD': 'postgre',
+        'HOST': 'localhost'
     }
 }
 
